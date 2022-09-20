1aba8a070f9b973a385df04e84027da6d8a87cdb
Filippo <filippo.mazzalai@gmail.com>
2022-09-20 14:48:26 +0200

    adding loginform

--- client/src/feature/auth/LoginForm.js
+++ client/src/feature/auth/LoginForm.js
@@ -1,11 +1,23 @@
 import "./auth.css";
+import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
 
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // if login is successfull
+    navigate("dashboard");
+  };
 
   return (
     <div className="login-form" >
-      <form>
+      <form onSumbit={handleLogin}>
+        <label htmlFor="email">Enter your email:</label><br>
+        <input type="email" id="email" name="email" placeholder="example@email.com" required />
+
+        <label for="pwd">Password:</label><br>
+        <input type="password" id="pwd" name="pwd" minlength="8" placeholder="password" required />
 
         <input type="submit" >Submit</input>
       </form>
