889dc953bb873fc999b1f7086aa5adeb584d715d
Filippo <filippo.mazzalai@gmail.com>
2022-10-11 16:31:33 +0200

    added session cookie properties

--- server/loaders/express.js
+++ server/loaders/express.js
@@ -18,7 +18,7 @@ module.exports = (app) => {
   // Parses urlencoded bodies
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  // ?
+  // trust client proxy
   app.set("trust proxy", 1);
 
   // Create session
@@ -29,7 +29,9 @@ module.exports = (app) => {
       saveUninitialized: false,
       cookie: {
         secure: false,
-        maxAge: 24*60*60*1000
+        maxAge: 24*60*60*1000,
+        secure: true,
+        sameSite: 'strict'
       }
     })
   );
